9f5197f0c018d636f859019b7ea43d1f4e6ba331
jeffdnganga <jeffdaniels950@gmail.com>
2022-08-14 23:27:10 +0300

    JEFF: Code clean up, Skills, and twitter link updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,31 +169,6 @@
   }
 
   /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
